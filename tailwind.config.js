af0430fd9ff92b14f2960ef26c419520601ae335
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-21 15:36:02 +0100

    tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./resources/**/*.blade.php"],
+    content: ['./resources/**/*.blade.php'],
     theme: {
         colors: {
-            transparent: "transparent",
-            current: "currentColor",
-            white: "#ffffff",
-            "base-100": "#FFF5F6",
-            "base-200": "#F7F5DD",
-            primary: "#000000",
-            secondary: "#F17581",
-            third: "#FF8BB2",
-            fourth: "#FFA7E4",
-            fifth: "#FFC6FF",
-            sixth: "#FFE4E5",
+            transparent: 'transparent',
+            current: 'currentColor',
+            white: '#ffffff',
+            'base-100': '#FFF5F6',
+            'base-200': '#F7F5DD',
+            primary: '#000000',
+            secondary: '#F17581',
+            third: '#FF8BB2',
+            fourth: '#FFA7E4',
+            fifth: '#FFC6FF',
+            sixth: '#FFE4E5',
         },
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/typography')],
 };
