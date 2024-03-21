5312289e5121f9b651601b5b20f8c8eb5526a293
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-21 15:52:24 +0100

    whoopsie daisy

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\QuibblerPost;
 
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('web');
+
 Route::get('blog/{quibbler_post:url}', function (QuibblerPost $quibbler_post) {
     return view('show-quibbler-post', compact('quibbler_post'));
 })
