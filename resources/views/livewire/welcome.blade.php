7d242380b76f80b63ef243bac513e654b0041978
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-22 17:30:52 +0100

    yes success

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -57,33 +57,57 @@
                 </div>
 
                 <div class="flex-1 px-6 py-8">
-                    <h3 class="mb-2 text-2xl font-semibold text-primary">
-                        Registriere dich
-                    </h3>
-                    <p>
-                        Und erhalte direkt eine E-Mail, sobald unser Shop online geht.
-                    </p>
+                    <div class="mb-2">
+                        <h3 class="mb-2 text-2xl font-semibold text-primary">
+                            Registriere dich
+                        </h3>
+                        <p>
+                            Und erhalte direkt eine E-Mail, sobald unser Shop online geht.
+                        </p>
+                    </div>
 
-                    <form wire:submit="save">
-                        <div class="py-4">
-                            <input type="text" id="email" name="email" wire:model="email"
-                                class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                                placeholder="deine@email.ch" required />
 
-                            @error('email')
-                                <span class="text-red-400">{{ $message }}</span>
-                            @enderror
+                    @if (session('success'))
+                        <div
+                            class="[&>svg]:text-foreground relative w-full rounded-lg border border-transparent bg-green-50 p-4 text-green-600 [&:has(svg)]:pl-11 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4">
+                            <svg class="h-5 w-5 -translate-y-0.5" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
 
+                            <h5 class="mb-1 font-medium leading-none tracking-tight">
+                                Erfolgreich angemeldet 🎉
+                            </h5>
+
+                            <div class="text-sm opacity-80">
+                                Du erhältst als Erster eine E-Mail, sobald unser Shop online geht.
+                            </div>
                         </div>
+                    @else
+                        <form wire:submit="save">
+                            <div class="py-4">
+                                <input type="text" id="email" name="email" wire:model="email"
+                                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                                    placeholder="deine@email.ch" required />
 
-                        <div class="flex justify-end">
-                            <button type="submit"
-                                class="focus:shadow-outline inline-flex items-center justify-center rounded-full bg-neutral-950 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-2">
-                                Jetzt registrieren
-                            </button>
-                        </div>
+                                @error('email')
+                                    <span class="text-red-400">{{ $message }}</span>
+                                @enderror
+                            </div>
 
-                    </form>
+                            <div class="flex justify-end">
+                                <button type="submit"
+                                    class="focus:shadow-outline inline-flex items-center justify-center rounded-full bg-neutral-950 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-2">
+                                    Jetzt registrieren
+                                </button>
+                            </div>
+                        </form>
+                    @endif
+
+
+
+
 
 
 
