1bfb0ca9144f8ccf191f3d43bfd4d096509c8b4a
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-22 17:12:54 +0100

    good enough

--- app/Livewire/Welcome.php
+++ app/Livewire/Welcome.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\NewsletterSubscription;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
-class Welcome extends Component
-{
-    public function render()
-    {
+class Welcome extends Component {
+    #[Validate('required|email:rfc|unique:newsletter_subscriptions,email')]
+    public string $email = '';
+
+    public function save() {
+        $this->validate();
+
+        NewsletterSubscription::create($this->only('email'));
+
+        $this->reset();
+
+        return redirect()->back()->with('success', 'Thank you for subscribing!');
+    }
+
+    public function render() {
         return view('livewire.welcome');
     }
 }
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -64,15 +64,20 @@
                         Und erhalte direkt eine E-Mail, sobald unser Shop online geht.
                     </p>
 
-                    <form action="">
+                    <form wire:submit="save">
                         <div class="py-4">
-                            <input type="text" id="email" name="email"
+                            <input type="text" id="email" name="email" wire:model="email"
                                 class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                                 placeholder="deine@email.ch" required />
+
+                            @error('email')
+                                <span class="text-red-400">{{ $message }}</span>
+                            @enderror
+
                         </div>
 
                         <div class="flex justify-end">
-                            <button type="button"
+                            <button type="submit"
                                 class="focus:shadow-outline inline-flex items-center justify-center rounded-full bg-neutral-950 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-2">
                                 Jetzt registrieren
                             </button>
