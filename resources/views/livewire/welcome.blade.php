06c6eccd469e0625a6aa017fd40098eaeec5ac82
Thomas Iseli <39917559+ThopQ@users.noreply.github.com>
2024-03-23 13:36:06 +0100

    loading state

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -103,7 +103,8 @@
 
                             <div class="flex justify-end">
                                 <button type="submit"
-                                    class="focus:shadow-outline inline-flex items-center justify-center rounded-full bg-neutral-950 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-2">
+                                    class="focus:shadow-outline inline-flex items-center justify-center rounded-full bg-neutral-950 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-2"
+                                    wire:loading.class="opacity-50">
                                     Jetzt registrieren
                                 </button>
                             </div>
